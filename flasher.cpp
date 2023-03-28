c19ecf221457f3fcf774691248b7c2a3808ad320
Mike McQuaid <mike@mikemcquaid.com>
2023-03-28 15:48:15 +0100

    flasher: download firmware from Rails.
    The old pages site can be now archived!

--- flasher.cpp
+++ flasher.cpp
@@ -1,37 +1,8 @@
 #include "flasher.h"
 #include "./ui_flasher.h"
 
-QString firmware_url =
-    "https://raisedevs.github.io/github_pages_firmware/firmware.bin";
-const char *firmware_url_root_ca_certificate =
-    "-----BEGIN CERTIFICATE-----\n"
-    "MIIEvjCCA6agAwIBAgIQBtjZBNVYQ0b2ii+nVCJ+xDANBgkqhkiG9w0BAQsFADBh\n"
-    "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
-    "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n"
-    "QTAeFw0yMTA0MTQwMDAwMDBaFw0zMTA0MTMyMzU5NTlaME8xCzAJBgNVBAYTAlVT\n"
-    "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxKTAnBgNVBAMTIERpZ2lDZXJ0IFRMUyBS\n"
-    "U0EgU0hBMjU2IDIwMjAgQ0ExMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKC\n"
-    "AQEAwUuzZUdwvN1PWNvsnO3DZuUfMRNUrUpmRh8sCuxkB+Uu3Ny5CiDt3+PE0J6a\n"
-    "qXodgojlEVbbHp9YwlHnLDQNLtKS4VbL8Xlfs7uHyiUDe5pSQWYQYE9XE0nw6Ddn\n"
-    "g9/n00tnTCJRpt8OmRDtV1F0JuJ9x8piLhMbfyOIJVNvwTRYAIuE//i+p1hJInuW\n"
-    "raKImxW8oHzf6VGo1bDtN+I2tIJLYrVJmuzHZ9bjPvXj1hJeRPG/cUJ9WIQDgLGB\n"
-    "Afr5yjK7tI4nhyfFK3TUqNaX3sNk+crOU6JWvHgXjkkDKa77SU+kFbnO8lwZV21r\n"
-    "eacroicgE7XQPUDTITAHk+qZ9QIDAQABo4IBgjCCAX4wEgYDVR0TAQH/BAgwBgEB\n"
-    "/wIBADAdBgNVHQ4EFgQUt2ui6qiqhIx56rTaD5iyxZV2ufQwHwYDVR0jBBgwFoAU\n"
-    "A95QNVbRTLtm8KPiGxvDl7I90VUwDgYDVR0PAQH/BAQDAgGGMB0GA1UdJQQWMBQG\n"
-    "CCsGAQUFBwMBBggrBgEFBQcDAjB2BggrBgEFBQcBAQRqMGgwJAYIKwYBBQUHMAGG\n"
-    "GGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBABggrBgEFBQcwAoY0aHR0cDovL2Nh\n"
-    "Y2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0R2xvYmFsUm9vdENBLmNydDBCBgNV\n"
-    "HR8EOzA5MDegNaAzhjFodHRwOi8vY3JsMy5kaWdpY2VydC5jb20vRGlnaUNlcnRH\n"
-    "bG9iYWxSb290Q0EuY3JsMD0GA1UdIAQ2MDQwCwYJYIZIAYb9bAIBMAcGBWeBDAEB\n"
-    "MAgGBmeBDAECATAIBgZngQwBAgIwCAYGZ4EMAQIDMA0GCSqGSIb3DQEBCwUAA4IB\n"
-    "AQCAMs5eC91uWg0Kr+HWhMvAjvqFcO3aXbMM9yt1QP6FCvrzMXi3cEsaiVi6gL3z\n"
-    "ax3pfs8LulicWdSQ0/1s/dCYbbdxglvPbQtaCdB73sRD2Cqk3p5BJl+7j5nL3a7h\n"
-    "qG+fh/50tx8bIKuxT8b1Z11dmzzp/2n3YWzW2fP9NsarA4h20ksudYbj/NhVfSbC\n"
-    "EXffPgK2fPOre3qGNm+499iTcc+G33Mw+nur7SpZyEKEOxEXGlLzyQ4UfaJbcme6\n"
-    "ce1XR2bFuAJKZTRei9AqPCCcUZlM51Ke92sRKw2Sfh3oius2FkOH6ipjv3U/697E\n"
-    "A7sKPPcw7+uvTPyLNhBzPvOk\n"
-    "-----END CERTIFICATE-----\n";
+const char* FLASHER_USER_AGENT = "Raise-Flasher-Qt";
+const char* DASHBOARD_RAISE_DEV_UPDATER_URL = "https://dashboard.raise.dev/updater";
 
 Flasher::Flasher(QWidget *parent)
     : QMainWindow(parent)
@@ -43,8 +14,7 @@ Flasher::Flasher(QWidget *parent)
     monospaceFont.setStyleHint(QFont::Monospace);
     ui->outputEdit->setFont(monospaceFont);
 
-    // TODO: use URL from Raise login
-    ui->firmwareUrlEdit->setText(firmware_url);
+    ui->firmwareUrlEdit->setText(DASHBOARD_RAISE_DEV_UPDATER_URL);
 
     cacheDir.setPath(QStandardPaths::writableLocation(QStandardPaths::CacheLocation));
     firmwareFile.setFileName(cacheDir.filePath("firmware/firmware.bin"));
@@ -126,6 +96,7 @@ void Flasher::downloadFirmware()
 
     QUrl firmwareUrl(ui->firmwareUrlEdit->text());
     QNetworkRequest firmwareRequest(firmwareUrl);
+    firmwareRequest.setHeader(QNetworkRequest::UserAgentHeader, FLASHER_USER_AGENT);
 
     setOutput(QString("Downloading %1...").arg(firmwareUrl.toString()));
 
